d0a0692f5f9acc70042d4d774c4886945c8c2b2d
Shuangbing <he.shuangbing@gmail.com>
2020-09-20 18:47:37 +0900

    add main controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { MemberController } from './member/member.controller';
+import { SubjectController } from './subject/subject.controller';
+import { WorkspaceController } from './workspace/workspace.controller';
+import { WorkspaceModule } from './workspace/workspace.module';
+import { UserModule } from './user/user.module';
+import { MemberModule } from './member/member.module';
+import { SubjectModule } from './subject/subject.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [WorkspaceModule, UserModule, MemberModule, SubjectModule],
+  controllers: [AppController, UserController, MemberController, SubjectController, WorkspaceController],
   providers: [AppService],
 })
 export class AppModule {}
