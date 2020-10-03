4666b13b0946a969f3983d73ab5eaab25ed790f2
Shuangbing <he.shuangbing@gmail.com>
2020-10-03 18:48:22 +0900

    fix databse url

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import { ClientModule } from './client/client.module';
 @Module({
   imports: [TypeOrmModule.forRoot({
     type: 'mysql',
-    url: process.env.DATABSE_URL || 'mysql://root:root@localhost:3306/iclass-dev',
+    url: process.env.DATABASE_URL || 'mysql://root:root@localhost:3306/iclass-dev',
     entities: ["dist/**/*.entity{.ts,.js}"],
     synchronize: true,
   }), UserModule, MemberModule, SubjectModule, GroupModule, AuthModule, ChatModule, ClientModule],
