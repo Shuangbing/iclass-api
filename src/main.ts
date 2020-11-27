815a588687987684c04793ec8cac838bb1454bd4
Shuangbing <he.shuangbing@gmail.com>
2020-11-27 14:45:15 +0900

    disable swagger on prod runtime

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-docs', app, document);
+  if (process.env.RUNTIME != 'PROD') SwaggerModule.setup('api-docs', app, document)
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.enableCors();
